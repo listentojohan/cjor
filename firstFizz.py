b2bcb835f3dbefe983c361eefd68068f28250d29
listentojohan <listentojohan@gmail.com>
2015-06-15 12:17:08 +0200

    First python

---
+++ firstFizz.py
@@ -0,0 +1,28 @@
+#fizz py
+
+# print "hello world"
+
+#foo = input("Enter input 1: ")
+#bar = input("Enter input 2: ")
+
+#for i in range(foo, bar):
+#	if i % 15 == 0:
+#		print "FizzBuzz"
+#	elif i % 3 == 0:
+#		print "Fizz"
+#	elif i % 5 == 0:
+#		print "Buzz"
+#	else:
+#		print i
+#print str(foo**2)
+def someName(arg1, arg2):
+	arg3 = arg1 * arg2
+	return arg1 + " times " + arg2 + " is " + arg3
+
+#foo = input("Type some value ")
+
+#someName(foo,foo*4)
+
+print someName(10, 15)
+
+
