82625b8221e122c41e444b4e0608e61940a9d5cd
listentojohan <listentojohan@gmail.com>
2015-12-14 23:29:08 +0100

    More changes

--- newsgroups/Main.py
+++ newsgroups/Main.py
@@ -3,8 +3,13 @@ __author__ = 'CJ'
 import nltk
 import random
 from collections import defaultdict
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#vectorizer = TfidfVectorizer()
 
 newsgroups = nltk.corpus.PlaintextCorpusReader('./data/', '.*/[0-9]+', encoding='latin1')
+#newsgroups = vectorizer.fit_transform(newsgroups)
 ids = newsgroups.fileids()
 
 random.seed(0)
@@ -17,6 +22,16 @@ trainSet = ids[int(size*0.1):]
 
 print("Length of training data: ", len(trainSet), "\nLength of test data: ", len(testSet))
 
+def vectorize(text):
+    vectorizer = feature_extraction.text.CountVectorizer(
+        stop_words='english',
+        ngram_range=(1, 1),  #ngram_range=(1, 1) is the default
+        dtype='double',
+        )
+    #text = [w.lower() for w in vectorizer]
+    return vectorizer.fit_transform(text)
+
+#print(vectorize(trainSet))
 
 def features(text):
     # Convert a post into a dictionary of features
@@ -35,83 +50,108 @@ def getclass(fileid):
 trainData = [(features(newsgroups.words(fileids=f)),getclass(f)) for f in trainSet]
 testData = [(features(newsgroups.words(fileids=f)),getclass(f)) for f in testSet]
 
-c = nltk.FreqDist(item[1] for item in trainData)
+# For vectorisation
+#trainData = [(vectorize(newsgroups.words(fileids=f)),getclass(f)) for f in trainSet]
+#testData = [(vectorize(newsgroups.words(fileids=f)),getclass(f)) for f in testSet]
+
+
+
+#c = nltk.FreqDist(item[1] for item in trainData)
 #default = c.keys()[0]
-default = (next(iter(c.values())))
+#default = (next(iter(c.values())))
 
-print("c: ", c)
+#print("c: ", c)
 
-sum(c == default for f, c in testData) / float(len(testData))
-
-# Naive Bayes
-nb = nltk.NaiveBayesClassifier.train(trainData)
-print("NB: ", nltk.classify.accuracy(nb, testData))
-
-
-from nltk.classify.scikitlearn import SklearnClassifier
-from sklearn.naive_bayes import BernoulliNB, GaussianNB
-# BernoulliNB
-bernoulli = SklearnClassifier(BernoulliNB())
-bernoulli.train(trainData)
-print("NB Bernoulli: ", nltk.classify.accuracy(bernoulli, testData))
-gaussian = SklearnClassifier(GaussianNB())
-gaussian.train(trainData.toarray(trainData))
-print("Gaussian: ", nltk.classify.accuracy(gaussian, testData))
-
-from sklearn.naive_bayes import MultinomialNB
-# MultinomialNB
-multi = SklearnClassifier(MultinomialNB())
-multi.train(trainData)
-print("NB Multinomial: ", nltk.classify.accuracy(multi, testData))
-
-from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.pipeline import Pipeline
-
-pipeline = Pipeline([('chi2', SelectKBest(chi2, k=1000)),
-                     ('nb', MultinomialNB())])
-
-pmulti = SklearnClassifier(pipeline)
-pmulti.train(trainData)
-print("NB Multinomial (pmulti): ", nltk.classify.accuracy(pmulti, testData))
+#sum(c == default for f, c in testData) / float(len(testData))
+def main():
+    # Naive Bayes
+    nb = nltk.NaiveBayesClassifier.train(trainData)
+    print("NB: ", nltk.classify.accuracy(nb, testData))
 
 
-from sklearn.metrics import f1_score
+    from nltk.classify.scikitlearn import SklearnClassifier
+    from sklearn.naive_bayes import BernoulliNB, GaussianNB
+    # BernoulliNB
+    bernoulli = SklearnClassifier(BernoulliNB())
+    bernoulli.train(trainData)
+    print("NB Bernoulli: ", nltk.classify.accuracy(bernoulli, testData))
+    #gaussian = SklearnClassifier(GaussianNB())
+    #gaussian.train(trainData.toarray(trainData))
+    #print("Gaussian: ", nltk.classify.accuracy(gaussian, testData))
 
-#results = pmulti.batch_classify(item[0] for item in testData)
-#results[:10]
-#print(f1_score([item[1] for item in testData], results))
+    from sklearn.naive_bayes import MultinomialNB
+    # MultinomialNB
+    multi = SklearnClassifier(MultinomialNB())
+    multi.train(trainData)
+    print("NB Multinomial: ", nltk.classify.accuracy(multi, testData))
 
-# Logistic Reggression
+    from sklearn.feature_selection import SelectKBest, chi2
+    from sklearn.pipeline import Pipeline
 
-from sklearn.linear_model import LinearRegression
+    pipeline = Pipeline([('chi2', SelectKBest(chi2, k=1000)),
+                         ('nb', MultinomialNB())])
 
-#linReg = SklearnClassifier(LinearRegression())
-#linReg.train((trainData))
-#print("Logistic Reggression: ", nltk.classify.accuracy(linReg, testData))
+    pmulti = SklearnClassifier(pipeline)
+    pmulti.train(trainData)
+    print("NB Multinomial (pmulti): ", nltk.classify.accuracy(pmulti, testData))
 
-# Ensembles
-# Random forest
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
-forest = SklearnClassifier(RandomForestClassifier())
-forest.train(trainData)
-print("Random Forest: ", nltk.classify.accuracy(forest, testData))
+    from sklearn.metrics import f1_score
 
-adaboost = SklearnClassifier(AdaBoostClassifier())
-adaboost.train(trainData)
-print("Adaboost: ", nltk.classify.accuracy(adaboost, testData))
+    #results = pmulti.batch_classify(item[0] for item in testData)
+    #results[:10]
+    #print(f1_score([item[1] for item in testData], results))
 
-#from sklearn.neural_network import
+    # Logistic Reggression
 
-# Support Vector Machines
+    from sklearn.linear_model import LinearRegression
 
-from sklearn.svm import LinearSVC
-from sklearn.metrics import confusion_matrix
+    #linReg = SklearnClassifier(LinearRegression())
+    #linReg.train((trainData))
+    #print("Logistic Reggression: ", nltk.classify.accuracy(linReg, testData))
 
-svm = SklearnClassifier(LinearSVC())
-svm.train(trainData)
+    # Ensembles
+    # Random forest
+    from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
-print("SVM: ", nltk.classify.accuracy(svm, testData))
+    forest = SklearnClassifier(RandomForestClassifier(n_estimators=100))
+    forest.train(trainData)
+    print("Random Forest: ", nltk.classify.accuracy(forest, testData))
+
+    adaboost = SklearnClassifier(AdaBoostClassifier())
+    adaboost.train(trainData)
+    print("Adaboost: ", nltk.classify.accuracy(adaboost, testData))
+
+    #from sklearn.neural_network import
+
+    # Support Vector Machines
+
+    from sklearn.svm import LinearSVC
+    from sklearn.metrics import confusion_matrix
+
+    svm = SklearnClassifier(LinearSVC())
+    svm.train(trainData)
+
+    print("SVM: ", nltk.classify.accuracy(svm, testData))
+
+'''
+# Some tests with other ways as representation
+def main2():
+    print("Using another representation: ")
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    vectorizer = TfidfVectorizer()
+    train = vectorizer.fit_transform(trainSet)
+    test = vectorizer.fit_transform(trainSet)
+    print("Data is loaded")
+
+    # SVM
+
+'''
+
+if __name__ == '__main__':
+    main()
+    #main2()
+
 
 '''
 results = svm.batch_classify(item[0] for item in testData)
